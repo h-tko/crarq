c441dab456f47f33ce1bb9a5faf6a12be0bad532
htakeo <bojan_krkic_esp@i.softbank.jp>
2016-04-21 01:59:06 +0000

    httpsを4433で待機するように

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,7 @@ use Mix.Config
 # with brunch.io to recompile .js and .css sources.
 config :child_rearing_question, ChildRearingQuestion.Endpoint,
   http: [port: 4000],
-  https: [port: 443,
+  https: [port: 4433,
           keyfile: Path.expand("priv/ssl/localhost.key"),
           certfile: Path.expand("priv/ssl/localhost.crt")],
   debug_errors: true,
