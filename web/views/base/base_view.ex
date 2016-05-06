5fcd65b558376af7b41031055190015d916c81da
hiroaki takeo <h-takeo@ip-172-31-20-216.ap-northeast-1.compute.internal>
2016-05-06 03:05:57 +0000

    任意の場所で改行するviewヘルパー追加

--- web/views/base/base_view.ex
+++ web/views/base/base_view.ex
@@ -3,6 +3,38 @@ defmodule ChildRearingQuestion.BaseView do
     quote do
 
       use ChildRearingQuestion.Web, :view
+
+      def add_br(text, length \\ 25) do
+        slice_and_add(text, "", length)
+      end
+
+      defp slice_and_add(remain, result, length) when remain != "" do
+
+        {bef, remain_res} = String.split_at(remain, length)
+
+        if String.contains? bef, "\r\n" do
+          data_list = String.split(bef, "\r\n")
+          remain = List.last(data_list) <> remain_res
+          last_index = Enum.count(data_list) - 1
+
+          data_list = List.delete_at(data_list, last_index)
+
+          bef_str = for data <- data_list do
+            data <> "\r\n"
+          end
+
+          result = result <> List.first(bef_str)
+        else
+            result = result <> "\r\n" <> bef
+            remain = remain_res
+        end
+
+        slice_and_add(remain, result, length)
+      end
+
+      defp slice_and_add(remain, result, length) do
+        result = result <> remain
+      end
     end
   end
 end
